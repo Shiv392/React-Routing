d14894c22fd5d50788bc496b2d527eac725ed76c
shivsoni <sonishiv309@gmail.com>
2023-04-27 23:42:36 +0530

    added Search Params

--- routing/src/components/Users.js
+++ routing/src/components/Users.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet ,useSearchParams} from 'react-router-dom';
 
 function Users() {
+    const [searchparams,setsearchparams]=useSearchParams();
+    const activeUser=searchparams.get('filter')==='active'
   return (
    <>
     <div>
@@ -10,7 +12,15 @@ function Users() {
       <h1>User3</h1>
 
     </div>
+    <div>
+        <button onClick={()=>setsearchparams({filter:'active'})} className='btn btn-primary'>Active Users</button>
+        <button onClick={()=>setsearchparams({})} className='btn btn-danger'>Reset Filter</button>
+   {
+    activeUser ? <h1>filter activated</h1> : <h1>not activated</h1>
+   }
+    </div>
     <Outlet></Outlet>
+    
    </>
   )
 }
