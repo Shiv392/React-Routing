622f13bc776299e93d25052a5d40ab119f02d0b1
shivsoni <sonishiv309@gmail.com>
2023-04-27 23:27:45 +0530

    URLS Params

--- routing/src/components/UserDetails.js
+++ routing/src/components/UserDetails.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function UserDetails() {
+const params=useParams();
+const id=params.id;
   return (
     <div>
-      <h1>Details About User</h1>
+      <h1>Details About User : {id}</h1>
     </div>
   )
 }
